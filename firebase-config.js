521acfca3b94880d38f8ffde9840c424979b7ab2
Abc's Noob <abcsspprt@gmail.com>
2025-05-27 11:32:49 +0700

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDtcs0XkNhf7yFRTzPY-A9WYet35YjQVT8",
+  authDomain: "abc-s-noob.firebaseapp.com",
+  databaseURL: "https://abc-s-noob-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "abc-s-noob",
+  storageBucket: "abc-s-noob.firebasestorage.app",
+  messagingSenderId: "196660846002",
+  appId: "1:196660846002:web:ce129820f388cc838658ab",
+  measurementId: "G-LG5SWH89MG"
+};
+// Khởi tạo Firebase
+export default firebaseConfig;
