b22c3f9029229a229f9548b2d18b23271c52d889
Abc's Noob <abcsspprt@gmail.com>
2025-05-27 11:38:54 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,17 +1,20 @@
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import firebase from "https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js";
+import "https://www.gstatic.com/firebasejs/8.10.1/firebase-auth.js";
+import "https://www.gstatic.com/firebasejs/8.10.1/firebase-firestore.js";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDtcs0XkNhf7yFRTzPY-A9WYet35YjQVT8",
   authDomain: "abc-s-noob.firebaseapp.com",
-  databaseURL: "https://abc-s-noob-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "abc-s-noob",
-  storageBucket: "abc-s-noob.firebasestorage.app",
+  storageBucket: "abc-s-noob.appspot.com",
   messagingSenderId: "196660846002",
   appId: "1:196660846002:web:ce129820f388cc838658ab",
   measurementId: "G-LG5SWH89MG"
 };
-// Khởi tạo Firebase
-export default firebaseConfig;
+
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+const db = firebase.firestore();
+
+export { firebase, auth, db };
