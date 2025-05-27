66fa90fd1dbb339f129be93c9eac5023905d4d8f
Abc's Noob <abcsspprt@gmail.com>
2025-05-27 11:45:53 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -2,19 +2,24 @@ import firebase from "https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js"
 import "https://www.gstatic.com/firebasejs/8.10.1/firebase-auth.js";
 import "https://www.gstatic.com/firebasejs/8.10.1/firebase-firestore.js";
 
+// cấu hình firebase
 const firebaseConfig = {
   apiKey: "AIzaSyDtcs0XkNhf7yFRTzPY-A9WYet35YjQVT8",
   authDomain: "abc-s-noob.firebaseapp.com",
+  databaseURL: "https://abc-s-noob-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "abc-s-noob",
-  storageBucket: "abc-s-noob.appspot.com",
+  storageBucket: "abc-s-noob.firebasestorage.app",
   messagingSenderId: "196660846002",
   appId: "1:196660846002:web:ce129820f388cc838658ab",
   measurementId: "G-LG5SWH89MG"
 };
 
-firebase.initializeApp(firebaseConfig);
+// khởi tạo app
+const app = firebase.initializeApp(firebaseConfig);
 
+// khởi tạo auth và firestore
 const auth = firebase.auth();
 const db = firebase.firestore();
 
-export { firebase, auth, db };
+// export
+export { auth, db, firebase };
